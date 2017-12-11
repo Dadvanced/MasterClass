b32ed1312fc50e10b9f9c542dcd1d3e84b1c372d
David León Galisteo <dadvanced@hotmail.es>
2017-12-11 21:04:48 +0100

    Add files via upload

---
+++ explicacionArrays.java
@@ -0,0 +1,39 @@
+/*
+ *  @author David León
+ */
+package ejerciciosjava;
+
+import java.util.Scanner;
+
+public class EjerciciosJava {
+
+    public static void main(String[] args) {
+
+        Scanner n = new Scanner(System.in);
+        int[] array = new int[10];
+        //cajoneras:                         0    |   1 |  2  |   3  |   4 |   5    | 6 ...7... 8... 9
+        //valor de las cajoneras: 212 | 22 | 32 | 53 | 42 | 4324| ......
+        
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 101);
+            System.out.print(array[i] + " ");
+        }
+
+        System.out.print("\nNúmero a buscar: ");
+        int numeroConcretoDeLaKaleeshi = Integer.parseInt(n.nextLine());
+
+        for (int i = 0; i < array.length; i++) {
+            if (i == numeroConcretoDeLaKaleeshi) { //preguntamos por el ÍNDICE
+                System.out.println("El número que se encuentra en el índice " + i + " es el : "  + array[i]);
+            }
+        }
+        
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == numeroConcretoDeLaKaleeshi) { //preguntamos por el VALOR del índice
+                System.out.println("El número se encuentra en el índice " + i);
+            }
+        }
+        
+        
+    }
+}
